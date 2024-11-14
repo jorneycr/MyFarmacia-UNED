50d728565dfe486a89f88c62433e9b293157fb2a
jorneycr <jorneycr@hotmail.com>
2024-11-14 08:48:36 -0600

    improvement for auth

--- myfarmacia-backend/controllers/userController.js
+++ myfarmacia-backend/controllers/userController.js
@@ -9,7 +9,7 @@ exports.registerUser = async (req, res) => {
         const existingUser = await User.findOne({ email });
         if (existingUser) return res.status(400).json({ message: "El usuario ya existe" });
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, 12);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
 
@@ -26,10 +26,10 @@ exports.loginUser = async (req, res) => {
         const user = await User.findOne({ email });
         if (!user) return res.status(400).json({ message: "Usuario no encontrado" });
 
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = await bcrypt.compareSync(password, user.password);
         if (!isMatch) return res.status(400).json({ message: "Contraseña incorrecta" });
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user._id }, 'LLAVESECRETA', { expiresIn: '1h' });
         res.json({ token, user });
     } catch (error) {
         res.status(500).json({ error: error.message });
